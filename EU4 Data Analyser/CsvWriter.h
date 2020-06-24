f9d8926fe3e817029e0267ab6ce082d4d84053ec
joshua_cottrell <joshua_cottrell@hotmail.co.uk>
2020-06-24 13:44:39 +0100

    Created the header files required  for the csv formatting

---
+++ EU4 Data Analyser/CsvWriter.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Writes the given text to a csv file specified by the file name
+void writeToCsv(const char* fileName, const char* text);
---
+++ EU4 Data Analyser/FileReader.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Extracts the lines from the file specified by the file name
+char* extractLines(const char* fileName);
+
+// Formats the given lines into csv text
+char* formatLines(char* lines);
